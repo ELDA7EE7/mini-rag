6988b9ecf3742f37f155f00aef2fd507d8fee2b2
ELDA7EE7 <mazenmohamedabd@gmail.com>
2026-04-29 00:54:12 +0300

    started integrating mongodb using Docker and added a scheme for chunks and project

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -7,6 +7,8 @@ class Settings(BaseSettings):
     FILE_MAXIMUM_SIZE:int
     FILE_DEFAULT_CHUNK_SIZE:int
 
+    MONGODB_URL:str
+    MONGODB_DATABASE:str
     class Config:
         env_file = ".env"
 
---
+++ src/models/db_schemes/project.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, Field, field_validator
+from typing import Optional
+from bson.objectid import ObjectId
+class Project(BaseModel):
+    _id: Optional[ObjectId] #this type is weird for pydantic so we need to resolve it
+    project_id: str = Field(...,min_length=1)
+
+    #custom validation
+    @field_validator('project_id')
+    def validate_project_id(cls,value):
+        if not value.isalnum():
+            raise ValueError('project_id must be alphanumeric')
+        return value
+
+    #this resolves the ObjectId pydantic problem
+    class Config:
+        arbitrary_types_allowed = True 
